a3ede66c98f8650f298670456008d885b2acd43b
AzureCloud9 <60143876+AzureCloud9@users.noreply.github.com>
2020-01-30 10:19:48 +0100

    Opdrachten af

---
+++ src/h02/Opdracht2p2.java
@@ -0,0 +1,20 @@
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+public class Opdracht2p2 extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+        setBackground(Color.blue);
+
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Marley", 50, 60 );
+        g.drawString("Mutange", 50,70);
+    }
+
+}
