d1c110bfd13c22af8239e6f317b95c849a3f798e
AzureCloud9 <60143876+AzureCloud9@users.noreply.github.com>
2020-02-04 09:24:47 +0100

    Opdrachten af

---
+++ src/h04/Vragen4.java
@@ -0,0 +1,25 @@
+/*
+1. Wat is een platform?
+A.combinatie van een processor en een besturingssysteem noemen we een platform
+
+2.Waarom hoeft een programmeur in Java maar één keer een programma te schrijven voor verschillende platforms?
+A. de JVM tijdens de uitvoering de applicatie of de applet de bytecode in machinetaal voor het betreffende platform.
+ In Java hoeft een programmeur dus maar één keer een applicatie of een applet te maken
+
+ 3. Wat is bytecode?
+ A. In Java wordt de broncode vertaald naar een tussentaal die standaard is. Deze tussentaal wordt Bytecode genoemd
+
+ 4. Wat doet JVM?
+ A. De JVM vertaalt tijdens de uitvoering de applicatie of de applet de bytecode in machinetaal voor het betreffende platform.
+
+ 5. Met welke methode kun je een vierkant tekenen?
+ A. (g.drawRect) (g.drawline) (g.fillrect)
+
+ 6. Noem drie methodes waarmee je een cirkel kunt tekenen
+ A. (g.drawOval)(g.fillOval)(g.drawArc)
+
+ 7. Waarom worden opdrachten voor het scherm in paint() gezet en niet in een andere methode?
+ A. Omdat het de graphic sector is/
+
+
+ */
