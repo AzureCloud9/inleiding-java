96fbacf605ee601eadc3f8f2edefac6faf85a508
AzureCloud9 <marleyalmere@gmail.com>
2020-12-07 20:17:07 +0100

    .,

--- src/h04/Opdracht4p2.java
+++ src/h04/Opdracht4p2.java
@@ -11,11 +11,11 @@ public class Opdracht4p2 extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.blue);
         g.setColor(Color.yellow);
-        setSize(400,400);
+        setSize(400, 400);
         g.drawRect(20, 100, 200, 150);
         g.drawRect(40, 180, 35, 70);
         g.drawRect(160, 150, 45, 45);
-        g.drawLine(185,150,185,195);
+        g.drawLine(185, 150, 185, 195);
 
 
     }
