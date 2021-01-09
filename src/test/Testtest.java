68c9dc95ab8956e18fb4393ba37e63ebed30f9b9
AzureCloud9 <marleyalmere@gmail.com>
2021-01-09 01:13:20 +0100

    ez

--- src/test/Testtest.java
+++ src/test/Testtest.java
@@ -1,41 +1,27 @@
+package h10;
 
-import java.applet.Applet;
 import java.awt.*;
+import java.applet.*;
 import java.awt.event.*;
 
-public class Testtest extends Applet {
-    TextField tekstvak;
-    Label label;
-    String s, tekst;
-    int jaartal;
+class GradeScore extends Applet {
+
 
     public void init() {
-        tekstvak = new TextField("", 20);
-        label = new Label("Type een jaartal en druk op enter");
-        tekstvak.addActionListener( new TekstvakListener() );
-        tekst = "";
-        add(label);
-        add(tekstvak);
+
     }
 
     public void paint(Graphics g) {
-        g.drawString(tekst, 50, 60 );
-    }
-
-    class TekstvakListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            s = tekstvak.getText();
-            jaartal = Integer.parseInt( s);
-            if ( (jaartal % 4 == 0 && !(jaartal % 100 == 0)) ||
-                    jaartal % 400 == 0 ) {
-                tekst = ""+ jaartal + " is een schrikkeljaar";
-            }
-            else {
-                tekst = ""+ jaartal + " is geen schrikkeljaar";
-            }
-            repaint();
+        int x = 50;
+        int y = 30;
+        int o = 1;
+        for (int i = 1; i < 30; i+=o ){
+            g.drawString(""+i, x, y);
+            x += 20;
+            o ++;
 
         }
+
+
     }
 }
-
