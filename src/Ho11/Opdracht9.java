2c9936a9c3750ace17e9d81de4f2ae6202786cc3
AzureCloud9 <marleyalmere@gmail.com>
2021-01-12 05:18:43 +0100

    .

---
+++ src/Ho11/Opdracht9.java
@@ -0,0 +1,28 @@
+package Ho11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht9 extends Applet {
+    Color black;
+    Color white;
+
+    public void init() {
+        setSize(500, 500);
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        int a = 50;
+        int b = 50;
+        int breedte = 40, hoogte = 40, row = 0, colom = 0;
+        black = (Color.black);
+        white = Color.white;
+        for (row = 0; row < 4; row++) {
+            a = 50;
+            b += 50;
+
+
+        }
+    }
+}
