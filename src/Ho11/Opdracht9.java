1ef19ce824d780a1cb828d71b3fc15e8034cc690
AzureCloud9 <marleyalmere@gmail.com>
2021-01-15 07:21:04 +0100

    .

--- src/Ho11/Opdracht9.java
+++ src/Ho11/Opdracht9.java
@@ -15,12 +15,40 @@ public class Opdracht9 extends Applet {
         setBackground(Color.blue);
         int a = 50;
         int b = 50;
-        int breedte = 40, hoogte = 40, row = 0, colom = 0;
-        black = (Color.black);
+        int side = 40, heigh = 50, row = 0, colomn = 0;
         white = Color.white;
+        black = (Color.black);
+
         for (row = 0; row < 4; row++) {
             a = 50;
             b += 50;
+
+            for (colomn = 0; colomn < 8; colomn++) {
+                if (colomn == 0 || colomn == 2 || colomn == 4 || colomn == 6) {
+                    g.setColor(black);
+                    g.fillRect(a, b, side, heigh);
+                } else {
+                    g.setColor(black);
+                    g.fillRect(a, b, heigh, side);
+                }
+
+                a += side;
+            }
+
+            //tweede rij
+            a = 50;
+            b += 50;
+            for (colomn = 0; colomn < 8; colomn++) {
+                if (colomn == 1 || colomn == 3 || colomn == 5 || colomn == 7) {
+                    g.setColor(black);
+                    g.fillRect(a, b, side, heigh);
+                } else {
+                    g.setColor(white);
+                    g.fillRect(a, b, heigh, side);
+                }
+
+                a += side;
+            }
 
 
         }
